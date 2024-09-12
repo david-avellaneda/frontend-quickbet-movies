d54d5bba54805956c48fcfa719fec88f2a1c52e1
David Avellaneda <avellaneda.developer@gmail.com>
2024-09-12 17:26:49 -0500

    Adding movie interfaces

---
+++ src/interfaces/movies.ts
@@ -0,0 +1,38 @@
+interface MovieResponse {
+	loading: boolean
+	data: MovieListResponse
+}
+
+interface MovieError {
+	error: boolean
+	message: string
+}
+
+interface MovieListResponse {
+	page: number
+	results: MovieDetails[]
+	total_pages: number
+	total_results: number
+	err?: boolean
+	err_msg?: string
+}
+
+interface MovieListProps {
+	title: string
+	classname: string
+	pageLink?: string
+	endpoint?: string
+	movies?: MovieListResponse
+}
+
+interface MovieDetails {
+	id: number
+	backdrop_path?: string
+	title: string
+	release_date: Date
+	vote_average: number
+	poster_path: string
+	genre_ids?: number[]
+}
+
+export type { MovieResponse, MovieError, MovieListResponse, MovieListProps, MovieDetails }
